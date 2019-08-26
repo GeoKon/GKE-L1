9be10dcf059b74c7f9bc5259a798d5f5df403c16
George Kontopidis <KONTOPIDIS@IEEE.ORG>
2019-08-25 22:34:14 -0400

    Instead of using B80 in stack, created compatible version with BUF using stack allocation. Use B80(x) syntax instead of B80 x

--- src/bufClass.h
+++ src/bufClass.h
@@ -24,7 +24,7 @@ class BUFBASE						// base class
 	char *quotes();
 	void print( const char *prompt="" );
 
-	char * operator !();    	// pointer to buffer
+	char * operator !();    	// pointer to buffer. To be deprecated
 	char *c_str();
 	size_t length();			// strlen()
     size_t size();				// max size
@@ -34,28 +34,35 @@ class BUFBASE						// base class
 class BUF: public BUFBASE
 {
 public:
-    BUF( int size )
+	// Usage: BUF t(100, "MyBuf")
+	// Allocates t as a buffer of 100 bytes, named MyBuf.
+	// If MyBuf starts with @, then alloc - dealloc messages are printed
+	BUF( int size, const char *label="" )
     {
         pntr = new char[ size ];
         maxsiz = size;
-		name = "";			
-		*pntr = 0;		
-    }
-	BUF( const char *first, int size=512 )
-    {
-        pntr = new char[ size ];
-        maxsiz = size;
-		name = first;		// for debugging purposes
-        copy( first );		// this also initializes BUF
+		name = label;					// for debugging purposes
+        *pntr = 0;
+		
 		if( *name==DEBUG_CHAR )
 			PF("Allocated %s[%d]\r\n", name, maxsiz );
+    }
+	// Usage: B80 s
+	// Defines a BUF in the stack of 80 bytes. No malloc
+	BUF( char *userstr, int size )		// user specified buffer. No alloc!
+    {
+        pntr = userstr;
+        maxsiz = size;
+		name = "$";				// $ indicates a user specified buffer
+		*pntr = 0;		
     }
     ~BUF()
     {
         if( maxsiz )			// this is required!
 		{
-			delete [] pntr;
 			maxsiz = 0;
+			if( *name != '$' )	// deallocate buffer only if allocated previously.
+				delete [] pntr;
 			if( *name==DEBUG_CHAR )
 				PF("Deallocated %s\r\n", name );
 		}
@@ -67,30 +74,12 @@ public:
     }
 };
 
-// -------------------------------- BUF located at the STACK --------------------------
-template < int T>
-class BUFT: public BUFBASE
-{
-private:
-    char s[T];
-public:
-    BUFT( const char* first="", int ignore = 0 ) 
-    {
-        pntr = s;
-        maxsiz = sizeof s;
-		name = first;		// for debugging purposes
-        copy( first );		// this also initializes BUF
-		if( *name==DEBUG_CHAR )
-			PF("Allocated %s[%d]\r\n", name, maxsiz );
-		
-    }
-	~BUFT()
-	{
-		maxsiz=0;
-	}
-};
+// Macros defining BUF in the stack
+#define  B32(A) char A##_str[32];  BUF A( A##_str, 32)
+#define  B64(A) char A##_str[64];  BUF A( A##_str, 64)
+#define  B80(A) char A##_str[80];  BUF A( A##_str, 80)
 
-#define B32 BUFT<32>
-#define B64 BUFT<64>
-#define B80 BUFT<80>
+#define B128(A) char A##_str[128]; BUF A( A##_str, 128)
+#define B256(A) char A##_str[256]; BUF A( A##_str, 256)
+
 
